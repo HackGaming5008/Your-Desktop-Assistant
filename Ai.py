d06e780665f2a0353f70e3090580525fb0adc7fe
HackGaming5008 <dgaming5008@gmail.com>
2026-07-01 20:01:59 +0530

    warped the ai function in a class

--- Ai.py
+++ Ai.py
@@ -6,70 +6,69 @@ from ui import ChatWindow
 with open("systemPrompt.txt", "r", encoding="utf-8") as file:
     sysPrompt = file.read()
 
-IP = "http://localhost:11434/api/chat"
-MODEL = "hermes3"
-SYSTEM = [
-    {
-        "role" : "system",
-        "content" : f"{sysPrompt}"
-
-    }
-]
-
-history = []
 
 def open_app(name):
     os.system(f'start "" "{name}"')
 
 
-def ask_ai():
-    response = requests.post(
-        IP,
-        json={
-            "model": MODEL,
-            "messages": SYSTEM + history[-50:],
-            "stream": False,
-            "keep_alive": "30m"
-        }
-    )
-
-    msg = response.json()["message"]["content"]
-
-    history.append({
-        "role": "assistant",
-        "content": msg
-    })
-
-    try:
-        data = json.loads(msg)
-        if data["action"] == "open_app":
-            app = data["app"]
-
-            open_app(app)
-            return f"Opening {app}"
-    except json.JSONDecodeError:
-        return msg
-        
-
 
 class AiAssistant():
     def __init__(self):
         super().__init__()
+        self.ip = "http://localhost:11434/api/chat"
+        self.model = "hermes3"
+        self.system = [
+            {
+                "role" : "system",
+                "content" : f"{sysPrompt}"
+
+            }
+        ]
+
+        self.history = []
+    
+
+    def ask_ai(self, Usermsg):
+
+        self.history.append({
+            "role": "user",
+            "content": Usermsg
+        })
+
+        response = requests.post(
+            self.ip,
+            json={
+                "model": self.model,
+                "messages": self.system + self.history[-50:],
+                "stream": False,
+                "keep_alive": "30m"
+            }
+        )
+
+        msg = response.json()["message"]["content"]
+
+        self.history.append({
+            "role": "assistant",
+            "content": msg
+        })
+
+        try:
+            data = json.loads(msg)
+            if data["action"] == "open_app":
+                app = data["app"]
+
+                open_app(app)
+                return  f"Opening {app}"
+        except json.JSONDecodeError:
+            return msg
+    
+
+
 
 
 
 
 while True:
     user = input("User: ")
-
-    history.append({
-        "role": "user",
-        "content": user
-    })
-
-    if user.lower() == "/bye":
-        break
-    elif user.lower() == "/clearhis":
-        history.clear()
-    else:
-        print(ask_ai())
+    assistant = AiAssistant()
+    print(assistant.ask_ai(user))
